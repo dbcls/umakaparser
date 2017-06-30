0209eea6ef032900e964eb9eb1283d9b235b6c47
fumiya-kubota <fumiya.kubota@glucose.jp>
2017-06-30 17:14:16 +0900

    bump 0.0.4

--- setup.py
+++ setup.py
@@ -34,6 +34,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: MIT License',
     ],
     entry_points="""
