9b88e3d7f4c07e6a51e58ddf867b1e58297d1bad
Riku Yamauchi <ripry258@gmail.com>
2021-01-18 12:11:17 +0900

    Update test module

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,9 +1,13 @@
+# coding:utf-8
+
+
 import pytest
 from click.testing import CliRunner
 from umakaparser.services import build
-from os.path import abspath, dirname
+from os import path, getenv
+import six
 
-FILE_DIR = dirname(abspath(__file__))
+FILE_DIR = path.dirname(path.abspath(__file__))
 FIXTURE_DIR = FILE_DIR + '/fixture'
 ASSETS_DIR = FIXTURE_DIR + '/meshdataset'
 TESTDATA_DIR = FIXTURE_DIR + '/validate'
@@ -16,10 +20,18 @@ def runner():
 
 
 def test_validate_metadata(runner):
-    result = runner.invoke(build, [])
-    assert 'Error: Missing argument' in result.output
+    LOCALE = getenv('LANG').split('_')[0]
 
-    TESTDATA_01 = TESTDATA_DIR + '/test.ttl'
-    DIST_FILE = DIST_DIR + '/test_01.json'
-    result = runner.invoke(build, [TESTDATA_01, '--assets', ASSETS_DIR, '--dist', DIST_FILE])
-    assert '>> ' + DIST_FILE in result.output
+    TESTDATA_01 = TESTDATA_DIR + '/success.ttl'
+    DIST_01 = DIST_DIR + '/success.json'
+    result = runner.invoke(build, [TESTDATA_01, '--assets', ASSETS_DIR, '--dist', DIST_01])
+    assert '>> ' + DIST_01 in result.output
+
+    TESTDATA_02 = TESTDATA_DIR + '/error_default_dataset.ttl'
+    DIST_02 = DIST_DIR + '/error_default_dataset.json'
+    result = runner.invoke(build, [TESTDATA_02, '--assets', ASSETS_DIR, '--dist', DIST_02])
+    assert '>> ' + DIST_02 not in result.output
+    assert result.exit_code == 2
+    assert 'Error: Validation failed.' in result.output
+    assert ('Cause: metadataに必須の' if LOCALE == 'ja' else 'Cause: defaultDataset') in result.output
+    assert not path.exists(DIST_02)
